13025dc5848cd2f06d21666ec70662772ec0bbe7
Cyrille Toulet <iwa@cloverleaf.(none)>
2014-02-11 20:32:58 +0100

    Documentation du ss-package logs

--- src/lille1/car2014/durieux_toulet/logs/LoggerUtilities.java
+++ src/lille1/car2014/durieux_toulet/logs/LoggerUtilities.java
@@ -6,19 +6,36 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.logging.SimpleFormatter;
 
+/**
+ * Logs manager
+ * 
+ * @author Durieux Thomas
+ * @author Toulet Cyrille
+ */
 public class LoggerUtilities {
+    /* Loggers */
 	private final Logger errorLogger;
 	private final Logger logLogger;
 	private final Logger debugLogger;
 
+    /* Instance */
 	private static LoggerUtilities INSTANCE;
 
+
+    /**
+     * Constructor
+     */
 	private LoggerUtilities() {
 		this.errorLogger = Logger.getLogger("errorLogger");
 		this.logLogger = Logger.getLogger("logLogger");
 		this.debugLogger = Logger.getLogger("debugLogger");
 	}
 
+
+    /**
+     * Get instance of LoggerUtilities
+     * @return LoggerUtilities singleton
+     */
 	private static LoggerUtilities getInstance() {
 		if (INSTANCE == null) {
 			INSTANCE = new LoggerUtilities();
@@ -26,6 +43,11 @@ public class LoggerUtilities {
 		return INSTANCE;
 	}
 
+
+    /**
+     * Log message
+     * @param message Message to log
+     */
 	public static void log(final String message) {
 		StackTraceElement[] stackTrace = Thread.currentThread().getStackTrace();
 		getInstance().logLogger.logp(Level.INFO, Thread.currentThread()
@@ -33,12 +55,23 @@ public class LoggerUtilities {
 				.getStackTrace()[2].getMethodName(), "[ LOG ] " + message);
 	}
 
+
+    /**
+     * Log error
+     * @param message Error message to log
+     */
 	public static void error(final String message) {
 		getInstance().errorLogger.logp(Level.SEVERE, Thread.currentThread()
 				.getStackTrace()[2].getClassName(), Thread.currentThread()
 				.getStackTrace()[2].getMethodName(), "[ERROR] " + message);
 	}
 
+
+    /**
+     * Log error
+     * @param message Error message to log
+     * @param eception Exception related to error message
+     */
 	public static void error(final String message, final Exception exception) {
 		getInstance().errorLogger.logp(Level.SEVERE, Thread.currentThread()
 				.getStackTrace()[2].getClassName(), Thread.currentThread()
@@ -46,15 +79,26 @@ public class LoggerUtilities {
 
 	}
 
+
+    /**
+     * Log error
+     * @param eception Exception to log
+     */
 	public static void error(final Exception exception) {
 		getInstance().errorLogger.logp(Level.SEVERE, Thread.currentThread()
 				.getStackTrace()[2].getClassName(), Thread.currentThread()
 				.getStackTrace()[2].getMethodName(), "[ERROR] N/A", exception);
 	}
 
+
+    /**
+     * Log debug message
+     * @param message Debug message to log
+     */
 	public static void debug(final String message) {
 		getInstance().debugLogger.logp(Level.INFO, Thread.currentThread()
 				.getStackTrace()[2].getClassName(), Thread.currentThread()
 				.getStackTrace()[2].getMethodName(), "[DEBUG] " + message);
 	}
+
 }
