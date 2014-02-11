b5e9d0589bec96e1588e5d0b5855f73e332485b0
Cyrille Toulet <iwa@cloverleaf.(none)>
2014-02-11 20:41:50 +0100

    Documentation du ss-package exception

--- src/lille1/car2014/durieux_toulet/exception/FTPClientException.java
+++ src/lille1/car2014/durieux_toulet/exception/FTPClientException.java
@@ -1,18 +1,44 @@
 package lille1.car2014.durieux_toulet.exception;
 
+/**
+ * Execption thrown by FTP Client
+ * 
+ * @author Durieux Thomas
+ * @author Toulet Cyrille
+ */
 public class FTPClientException extends Exception {
+
+    /**
+     * Constructor
+     */
 	public FTPClientException() {
 		super();
 	}
 
+
+    /**
+     * Constructor
+     * @param message Message of exception
+     */
 	public FTPClientException(final String message) {
 		super(message);
 	}
 
+
+    /**
+     * Constructor
+     * @param t Throwable exception
+     */
 	public FTPClientException(final Throwable t) {
 		super(t);
 	}
 
+
+    /**
+     * Constructor
+     * @param message Message of exception
+     * @param t Throwable exception
+     */
 	public FTPClientException(final String message, final Throwable t) {
 		super(message, t);
 	}
--- src/lille1/car2014/durieux_toulet/exception/RequestHandlerException.java
+++ src/lille1/car2014/durieux_toulet/exception/RequestHandlerException.java
@@ -1,18 +1,44 @@
 package lille1.car2014.durieux_toulet.exception;
 
+/**
+ * Execption thrown by Request Handler
+ * 
+ * @author Durieux Thomas
+ * @author Toulet Cyrille
+ */
 public class RequestHandlerException extends Exception {
+
+	/**
+	 * Constructor
+	 */
 	public RequestHandlerException() {
 		super();
 	}
 
+
+	/**
+	 * Constructor
+	 * @param message Message of exception
+	 */
 	public RequestHandlerException(final String message) {
 		super(message);
 	}
 
+
+	/**
+	 * Constructor
+	 * @param t Throwable exception
+	 */
 	public RequestHandlerException(final Throwable t) {
 		super(t);
 	}
 
+
+	/**
+	 * Constructor
+	 * @param message Message of exception
+	 * @param t Throwable exception
+	 */
 	public RequestHandlerException(final String message, final Throwable t) {
 		super(message, t);
 	}
